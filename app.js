ffec6b63f1a3b4af6c19914e36195f06dd9dcf31
Alexander Þór Hafþórsson <athh15@Alexanders-MacBook-Pro.local>
2019-01-25 16:31:56 +0000

    npm start

--- app.js
+++ app.js
@@ -40,5 +40,5 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 app.listen(port, hostname, () => {
-  console.info(`Server running at http://$(hostname}:${port}/`);
+  console.info(`Server running at http://${hostname}}:${port}/`);
 });
